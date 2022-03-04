ec5b4ded4a1726a07bd262d1f0e4b46cafc31c0d
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-03-04 22:49:22 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,3 +1,35 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@AbdulRiyaz16 
+clean-code-craft-tcq-3
+/
+spring-in-c-AbdulRiyaz16
+Public
+generated from clean-code-craft-tcq-1/statisact-c
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+spring-in-c-AbdulRiyaz16/stats.c
+@AbdulRiyaz16
+AbdulRiyaz16 Update stats.c
+Latest commit 815f25e 1 minute ago
+ History
+ 2 contributors
+@AbdulRiyaz16@github-classroom
+58 lines (53 sloc)  1.02 KB
+   
 #include "stats.h"
 #include <math.h>
 #ifdef NAN
@@ -15,15 +47,16 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     //s.max = 0;
     float max,min,avg;
     float sum;
-    max= numberset[0];
-    min=numberset[0];
-    sum=numberset[0];
     if(setlength==0)
     {
     	return s;
     }
     else
     {
+    max= numberset[0];
+    min=numberset[0];
+    sum=numberset[0];
+    
 		
     for (int i = 1; i < setlength; i++)
 	{
@@ -31,12 +64,12 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 		{
 			max = numberset[i];
 		}
-        	if(numberset[i] < min)
-        	{
-            		min=numberset[i];
-              	}
-       		sum=sum+numberset[i];
-        }
+        if(numberset[i] < min)
+        {
+            min=numberset[i];
+       	}
+      	sum=sum+numberset[i];
+    }
 	avg=sum/setlength;
 	s.average=avg;
 	s.max=max;
@@ -47,12 +80,3 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
-
-/*int main()
-{
-    Stats computedStats;
-    float numberset[] = {1.5, 8.9, 3.2, 4.5};
-    int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    computedStats = compute_statistics(numberset, setlength);
-return 0;
-}*/
