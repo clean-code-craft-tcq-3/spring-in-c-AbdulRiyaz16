85bedf3716e050b2b5f6aeb1ee518fc57dafd1cb
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-03-04 21:59:48 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-struct {
+struct Stats{
     float average;
     float min;
     float max;
 
-} Stats;
+};
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
